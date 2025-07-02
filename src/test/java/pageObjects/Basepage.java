dbc06d8601bc9f5e955bd3d3a822100550bd4a53
Tembhurnikar <2408394@cognizant.com>
2025-07-02 15:27:43 +0530

    Firstpage to Basepage

---
+++ src/test/java/pageObjects/Basepage.java
@@ -0,0 +1,14 @@
+package pageObjects;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+
+public class firstpage {
+	WebDriver driver;
+	public firstpage(WebDriver driver) {
+		// TODO Auto-generated constructor stub
+		this.driver=driver;
+		PageFactory.initElements(driver, this);
+	}
+
+}
